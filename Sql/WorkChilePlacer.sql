389bc58d0fdd624e911ad1a3ad04cf834858a795
EfrainMejiasC <efrainmejiasc@hotmail.com>
2021-05-06 17:59:27 -0400

    Update WorkChilePlacer.sql

--- Sql/WorkChilePlacer.sql
+++ Sql/WorkChilePlacer.sql
@@ -31,6 +31,6 @@ SELECT * FROM ChilePlacer..TypeNacionalidad
 --TRUNCATE TABLE ChilePlacer..TypeGirls
 --DELETE ChilePlacer..Girls Where Id > 1
  --UPDATE ChilePlacer..Girls set Activo = 0 
-  --UPDATE ChilePlacer..TypeNacionalidad set Nacionalidad = 'Chile', Ide = 'Chile' 
-  --UPDATE ChilePlacer..TypeDrink set Drink= 'Ocacionalmente', Ide = 'Ocacionalmente' Where Id = 3 
+ --UPDATE ChilePlacer..TypeNacionalidad set Nacionalidad = 'Chile', Ide = 'Chile' 
+--UPDATE ChilePlacer..TypeDrink set Drink= 'Ocacionalmente', Ide = 'Ocacionalmente' Where Id = 3 
 --UPDATE ChilePlacer..Girls Set password ='ZWZyYWlubWVqaWFzY0Bob3RtYWlsLmNvbSMxMjM0'
