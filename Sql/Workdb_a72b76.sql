3d0e96d2147366d9156123261883e6789611bb56
EfrainMejiasC <efrainmejiasc@hotmail.com>
2021-05-26 18:02:25 -0400

    Update Workdb_a72b76.sql

--- Sql/Workdb_a72b76.sql
+++ Sql/Workdb_a72b76.sql
@@ -13,3 +13,7 @@ SELECT * FROM db_a72b76_chileplacer..AppLog
 --TRUNCATE TABLE db_a72b76_chileplacer..ProfileGirls
 --TRUNCATE TABLE db_a72b76_chileplacer..Girls
 --Update db_a72b76_chileplacer..ProfileGirls  set Path='' Where id = 2 
+
+
+SELECT * FROM  db_a72b76_chileplacer..UserAdm
+--INSERT INTO db_a72b76_chileplacer..USERADM VALUES ('efrainmejiasc@gmail.com','ZWZyYWlubWVqaWFzY0BnbWFpbC5jb20jMTIzNE1pbGlzZW4=',1,GetDate(),'root')
